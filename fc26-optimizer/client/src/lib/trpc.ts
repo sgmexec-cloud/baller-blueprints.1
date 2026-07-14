63dc24075d02f8da5cb10e91158f7dce2bd71f57
sgmexec-cloud <sgmexec@googlemail.com>
2026-07-14 08:44:19 +0100

    Update trpc.ts

--- fc26-optimizer/client/src/lib/trpc.ts
+++ fc26-optimizer/client/src/lib/trpc.ts
@@ -1,4 +1,15 @@
 import { createTRPCReact } from "@trpc/react-query";
+import { httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "../../../server/routers";
 
 export const trpc = createTRPCReact<AppRouter>();
+
+// This client configuration ensures your login cookie is sent to the server
+export const trpcClient = trpc.createClient({
+  links: [
+    httpBatchLink({
+      url: "/api/trpc",
+      credentials: "include", // <--- This allows the cookie to be sent
+    }),
+  ],
+});
