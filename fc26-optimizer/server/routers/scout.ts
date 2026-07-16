9e905d8104ad87bdb432bdac292aacb748446023
sgmexec-cloud <sgmexec@googlemail.com>
2026-07-16 22:51:13 +0100

    Update scout.ts

--- fc26-optimizer/server/routers/scout.ts
+++ fc26-optimizer/server/routers/scout.ts
@@ -1,4 +1,4 @@
-9import { z } from "zod";
+import { z } from "zod";
 import { publicProcedure, router } from "../_core/trpc";
 import { invokeLLM } from "../_core/llm";
 import { getScoutingContext } from "../csvLoader";
